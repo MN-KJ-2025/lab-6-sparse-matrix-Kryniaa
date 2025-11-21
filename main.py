390b2a034da9cc37235ee533b47d2a37f8c5a143
Jakub Koclega <krynia@student.agh.edu.pl>
2025-11-21 22:57:32 +0100

    Sprawozdanie 6 i zadania

--- main.py
+++ main.py
@@ -12,32 +12,24 @@ import scipy as sp
 
 
 def is_diagonally_dominant(A: np.ndarray | sp.sparse.csc_array) -> bool | None:
-    """Funkcja sprawdzająca czy podana macierz jest diagonalnie zdominowana.
-
-    Args:
-        A (np.ndarray | sp.sparse.csc_array): Macierz A (m,m) podlegająca 
-            weryfikacji.
-    
-    Returns:
-        (bool): `True`, jeśli macierz jest diagonalnie zdominowana, 
-            w przeciwnym wypadku `False`.
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
-    pass
+    if not isinstance(A, (np.ndarray, sp.sparse.csc_array)):
+        return None
+    if A.ndim != 2 or A.shape[0] != A.shape[1]:
+        return None
+    diagonals =np.abs(A.diagonal())
+    var = np.sum(np.abs(A),axis=1) - diagonals
+    if(np.all(var<diagonals)):
+        return True
+    else:
+        return False
 
 
 def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
-    """Funkcja obliczająca normę residuum dla równania postaci: 
-    Ax = b.
-
-    Args:
-        A (np.ndarray): Macierz A (m,n) zawierająca współczynniki równania.
-        x (np.ndarray): Wektor x (n,) zawierający rozwiązania równania.
-        b (np.ndarray): Wektor b (m,) zawierający współczynniki po prawej 
-            stronie równania.
-    
-    Returns:
-        (float): Wartość normy residuum dla podanych parametrów.
-        Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
-    """
-    pass
+    if not isinstance(A, np.ndarray) or not isinstance(x, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
+    if A.ndim != 2 or x.ndim != 1 or b.ndim != 1:
+        return None
+    if A.shape[0] != b.shape[0] or A.shape[1] != x.shape[0]:
+        return None
+    r = A @ x - b
+    return np.linalg.norm(r)
